e11d8ceb1d5352529c262de27bba13f831865f4e
rupert-jones-99 <netflicks65@gmail.com>
2025-06-09 19:15:58 +0200

    routing

---
+++ app/(tabs)/detail.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+interface DetailProps{
+    state: boolean;
+    created: Date; 
+    name: string;
+}
+export default function Detail(props: DetailProps){
+    return(<View>
+        
+    </View>)
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return   (
+    <>      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" /></>
+  )
 }
